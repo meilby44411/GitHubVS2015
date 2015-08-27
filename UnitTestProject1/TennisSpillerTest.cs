556d1071a56e655254429d7532bcd99ecb58fde3
Lasse Meilby <lassemeilby444@hotmail.com>
2015-08-27 09:17:00 +0200

    setup 2 tests

---
+++ UnitTestProject1/TennisSpillerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using trådopgave;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class TennisSpillerTest {
+        [TestMethod]
+        public void TestNyTennisSpillerFrederikIkkeHarBolden() {
+            //3 Setup
+            TennisSpiller Frederik;
+            const string navn = "Frederik";
+            //2 Execution
+            Frederik = new TennisSpiller(navn);
+            //1 Verification
+            Assert.AreEqual(navn, Frederik.Navn);
+            Assert.IsFalse(Frederik.HarBolden);
+            //4 Teardown
+        }
+        [TestMethod]
+        public void TestNyTennisSpillerCarolineIkkeHarBolden() {
+            //3 Setup
+            TennisSpiller Caroline;
+            const string navn = "Caroline";
+            //2 Execution
+            Caroline = new TennisSpiller(navn);
+            //1 Verification
+            Assert.AreEqual(navn, Caroline.Navn);
+            Assert.IsFalse(Caroline.HarBolden);
+            //4 Teardown
+        }
+
+
+    }
+}
---
+++ trådopgave/TennisSpiller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trådopgave {
+    public class TennisSpiller {
+
+        public TennisSpiller(string Navn) {
+            this.Navn = Navn;
+        }
+        public bool HarBolden { get; set; }
+        public string Navn { get; set; }
+
+    }
+}
